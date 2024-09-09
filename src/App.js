64e09c79fae03dadcbf6d0890e598ef4be6ee942
Ugochika <ugochikajnr@gmail.com>
2024-09-09 11:09:40 +0100

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState }from 'react'
 
 function App() {
+  // an Array that will store the todos
+  // Keep track of what the todo is
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+
+    // Add the todo to the list
+    setList([...list, newTodo]);
+
+    // Clear input box
+    setInput("");
+  };
+  // Function to delete Todos
+  const deleteTodo = (id) => {
+    // Filter out todo with the id
+    const newList = list.filter((todo) => todo.id !== id);
+
+    setList(newList);
+  }; 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <input type="text" 
+      // Bind the input variable with the input field
+      value={input} 
+      // Listen for ant characters and set value of the input state variable
+      onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => addTodo(input)}>Add</button>
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo}
+            <button onClick={() => deleteTodo(todo.id)}>&times;</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
