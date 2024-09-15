1b477574cb8e3c501ad8e78f68177a0c36785a04
Ugochika <ugochikajnr@gmail.com>
2024-09-15 12:46:31 +0100

    todo with mark as completed

--- src/App.js
+++ src/App.js
@@ -1,52 +1,72 @@
-
-import React, { useState }from 'react'
+import React, { useState } from 'react';
+import './App.css'; 
 
 function App() {
-  // an Array that will store the todos
-  // Keep track of what the todo is
-  const [list, setList] = useState([]);
+  // Declaring a state variable for input and initializing it with an empty string
   const [input, setInput] = useState("");
 
+  // Declaring a state variable 'todos' and initializing it with a single todo item
+  const [todos, setTodos] = useState([
+    {
+      id: Math.random(), // Each todo has a unique ID
+      text: "This is a simple todo", // The todo text
+      isDone: false // Marks whether the task is done
+    }
+  ]);
+
+  // Function to add a new todo item
   const addTodo = (todo) => {
     const newTodo = {
-      id: Math.random(),
-      todo: todo,
+      id: Math.random(), // Generating a unique ID for the new todo
+      text: todo, // Setting the text from the input
+      isDone: false // Newly added todos are not done by default
     };
-
-    // Add the todo to the list
-    setList([...list, newTodo]);
-
-    // Clear input box
-    setInput("");
+    setTodos([...todos, newTodo]); // Adding the new todo to the existing list
+    setInput(""); // Clearing the input field after adding
   };
-  // Function to delete Todos
-  const deleteTodo = (id) => {
-    // Filter out todo with the id
-    const newList = list.filter((todo) => todo.id !== id);
 
-    setList(newList);
-  }; 
+  // Function to delete a todo by filtering it out based on its ID
+  const deleteTodo = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id); // Filtering out the todo with the given ID
+    setTodos(newTodos); // Updating the state with the new list
+  };
+
+  // Function to mark a todo as done/undone
+  const markTodo = (id) => {
+    const newTodos = todos.map(todo =>
+      todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+    ); // Toggling the isDone status for the matched todo
+    setTodos(newTodos); // Updating the state with the new list
+  };
 
   return (
-    <div>
-      <h1>Todo List</h1>
-      <input type="text" 
-      // Bind the input variable with the input field
-      value={input} 
-      // Listen for ant characters and set value of the input state variable
-      onChange={(e) => setInput(e.target.value)}
+    <div className="app">
+      <h1 className="text-center mb-4">Todo List</h1>
+      {/* Input field for new todos */}
+      <input
+        type="text"
+        value={input} // Binding the input state
+        onChange={(e) => setInput(e.target.value)} // Updating the input value as the user types
       />
+      {/* Button to add a new todo */}
       <button onClick={() => addTodo(input)}>Add</button>
+      
+      {/* Displaying the list of todos */}
       <ul>
-        {list.map((todo) => (
-          <li key={todo.id}>
-            {todo.todo}
+        {todos.map((todo) => (
+          <li key={todo.id} className={todo.isDone ? "doneTask" : ""}>
+            {todo.text} {/* Showing the text of the todo */}
+            {/* Button to toggle between done/undone */}
+            <button onClick={() => markTodo(todo.id)}>
+              {todo.isDone ? "Undo" : "Done"} {/* Label changes based on isDone status */}
+            </button>
+            {/* Button to delete the todo */}
             <button onClick={() => deleteTodo(todo.id)}>&times;</button>
           </li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
